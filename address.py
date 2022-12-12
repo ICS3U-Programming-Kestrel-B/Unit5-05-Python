bb67b4fa7886572cd3778136b0ea00d67a2233e2
Kestrel Bryce <Madeline.Bryce@stu.ocsb.ca>
2022-12-12 01:50:49 +0000

    Finished Python

--- address.py
+++ address.py
@@ -24,33 +24,29 @@ def main():
     # initializing user_apartment
     user_apartment = input("Enter your apartment number (n/a if no apartment): ")
     # initializing user_street_name
-    user_street_name = input("Enter your street name: ")
+    user_street_name = input("Enter your street name (e.x: sesame st): ")
     # initializing user_street_number
     user_street_number = input("Enter your street number: ")
     # initializing user_city
     user_city = input("Enter your city: ")
     # initializing user_province
-    user_province = input("Enter your province (shortened, AB, ON, etc): ")
+    user_province = input("Enter your province (e.x: AB, ON): ")
     # initializing user_postal
-    user_postal = input("Enter your postal code: ")
+    user_postal = input("Enter your postal code (e.x: K1S 5P4): ")
 
     # making calculate function
-    def format_address():
+    def format_address(
+        user_f_name,
+        user_l_name,
+        user_apartment,
+        user_street_number,
+        user_street_name,
+        user_city,
+        user_province,
+        user_postal,
+    ):
         # making user_full_name
-        user_full_name = (
-            user_f_name.upper()
-            + " "
-            + user_l_name.upper(
-                user_f_name,
-                user_l_name,
-                user_apartment,
-                user_street_number,
-                user_street_name,
-                user_city,
-                user_province,
-                user_postal,
-            )
-        )
+        user_full_name = user_f_name.upper() + " " + user_l_name.upper()
         if user_apartment != "n/a":
             user_address = (
                 user_full_name
@@ -59,11 +55,11 @@ def main():
                 + "-"
                 + user_street_number
                 + " "
-                + user_street_name
+                + user_street_name.upper()
                 + " "
-                + user_city
+                + user_city.upper()
                 + " "
-                + user_province
+                + user_province.upper()
                 + " "
                 + user_postal
             )
@@ -73,11 +69,11 @@ def main():
                 + "\n"
                 + user_street_number
                 + " "
-                + user_street_name
+                + user_street_name.upper()
                 + " "
-                + user_city
+                + user_city.upper()
                 + " "
-                + user_province
+                + user_province.upper()
                 + " "
                 + user_postal
             )
